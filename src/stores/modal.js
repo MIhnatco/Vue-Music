ac0e259c4b22518a932de8e8ccf0707f6a889bd6
Ihliana <Ihnatco@protonmail.com>
2024-01-15 14:42:05 +0100

    Update AppAuth.vue
    using aliases and showing tabs for forms

--- src/stores/modal.js
+++ src/stores/modal.js
@@ -3,7 +3,7 @@ import {defineStore} from 'pinia'
 export default defineStore('modal', {
     state: () => ({
         isOpen: false,
-    }), 
+    }),  
     getters: {
         hiddenClass(state){
             return !state.isOpen ? "hidden" : "";
