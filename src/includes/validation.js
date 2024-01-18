80e08c3fc653781d49934d4eccc670cc7b41aa50
Ihliana <Ihnatco@protonmail.com>
2024-01-18 09:29:44 +0100

    update validation.js
    creating validation triggers and alerts for submission

--- src/includes/validation.js
+++ src/includes/validation.js
@@ -1,6 +1,21 @@
-import { Form as VeeForm, Field as VeeField, defineRule, ErrorMessage, configure } from 'vee-validate'
-import { required, min, max, alpha_spaces as alphaSpaces, email, min_value as minVal, max_value as maxVal,
-  confirmed, not_one_of as excluded } from '@vee-validate/rules'
+import {
+  Form as VeeForm,
+  Field as VeeField,
+  defineRule,
+  ErrorMessage,
+  configure
+} from 'vee-validate'
+import {
+  required,
+  min,
+  max,
+  alpha_spaces as alphaSpaces,
+  email,
+  min_value as minVal,
+  max_value as maxVal,
+  confirmed,
+  not_one_of as excluded
+} from '@vee-validate/rules'
 
 export default {
   install(app) {
@@ -14,32 +29,36 @@ export default {
     defineRule('max', max)
     defineRule('alpha_spaces', alphaSpaces)
     defineRule('email', email)
-    defineRule("min_value", minVal)
-    defineRule("max_value", maxVal)
-    defineRule('password_mismatch', confirmed) 
+    defineRule('min_value', minVal)
+    defineRule('max_value', maxVal)
+    defineRule('password_mismatch', confirmed)
     defineRule('excluded', excluded)
     defineRule('country_excluded', excluded)
 
     configure({
       generateMessage: (ctx) => {
         const messages = {
-          required: `The field ${ctx.field} is required`, 
-          min: `The field ${ctx.field} is too short`, 
-          max: `The field ${ctx.field} is too long`, 
-          alpha_spaces: `The field ${ctx.field} may only contain alphabetical characters and space`, 
+          required: `The field ${ctx.field} is required`,
+          min: `The field ${ctx.field} is too short`,
+          max: `The field ${ctx.field} is too long`,
+          alpha_spaces: `The field ${ctx.field} may only contain alphabetical characters and space`,
           email: `The field ${ctx.field} must be a valid email`,
-          min_value: `The field ${ctx.field} is too low.`, 
+          min_value: `The field ${ctx.field} is too low.`,
           max_value: `The field ${ctx.field} is too high.`,
-          excluded: `You are not allowed to use this value for the field ${ctx.field}.` ,
+          excluded: `You are not allowed to use this value for the field ${ctx.field}.`,
           country_excluded: `Due to restrictions, we do not accept users from this location.`,
           password_mismatch: `The passwords don't match.`,
-          tos: "You must accept the Terms of Service"
-
-        };
-        const message =  messages[ctx.rule.name] ? messages[ctx.rule.name] : `The field ${ctx.field} is invalid`;
-        return message;
-
-      }
+          tos: 'You must accept the Terms of Service'
+        }
+        const message = messages[ctx.rule.name]
+          ? messages[ctx.rule.name]
+          : `The field ${ctx.field} is invalid`
+        return message
+      },
+      validateOnBlur: true,
+      validateOnChange: true,
+      validateOnInput: false,
+      validateOnModelUpdate: true
     })
   }
 }
