7dad8bfbaeda176fd3ddc505d2d298028659b53b
Ihliana <Ihnatco@protonmail.com>
2024-01-22 13:19:21 +0100

    Update user.js
    connecting a user to the database

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -110,7 +110,8 @@
 
 <script>
 
-  import firebase from '@/includes/firebase'
+import { mapActions } from 'pinia'
+import useUserStore from '@/stores/user'
 
 export default {
   name: 'RegisterForm',
@@ -139,26 +140,33 @@ export default {
     }
   },
   methods: {
+    ...mapActions(useUserStore, {
+      createUser: 'register'
+    }),
     async register(values) {
       this.reg_show_alert = true
       this.reg_in_submission = true
       this.reg_alert_variant = 'bg-blue-500'
       this.reg_alert_message = 'Please wait! Your account is being created.'
 
-      let userCred = null;
+  
 
-      try{
-        userCred = await firebase.auth().createUserWithEmailAndPassword( values.email, values.password)
-      } catch(error){
-        this.reg_in_submission = false;
-        this.reg_alert_variant = 'bg-red-500';
-        this.reg_alert_message = "An unexpected error occured. Please try again later."
-        return;
+      try {
+        await this.createUser(values)
+        
+      } catch (error) {
+        this.reg_in_submission = false
+        this.reg_alert_variant = 'bg-red-500'
+        this.reg_alert_message = 'An unexpected error occured. Please try again later.'
+        return
       }
+
+
+
 
       this.reg_alert_variant = 'bg-green-500'
       this.reg_alert_message = 'Success! Your account has been created.'
-      console.log(userCred)
+ 
     }
   }
 }
--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,6 +1,8 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
 
+import 'firebase/firestore'
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: 'AIzaSyBDwp6WitDfOqcWIbLDWFkCmjPh8fR7_8Y',
@@ -11,4 +13,25 @@ const firebaseConfig = {
   appId: '1:59660704910:web:7887ff4506a69c4bc8fddf'
 }
 
-export default firebase.initializeApp(firebaseConfig)
+
+//Initialize firebase
+ firebase.initializeApp(firebaseConfig)
+
+ //
+ const auth = firebase.auth()
+
+ //database reference 
+ //object with methods and properties to interact with database 
+ const db = firebase.firestore()
+
+ //object contains the name of the database
+ //we export it to be accessible in multiple components
+ //this function returns an object with methods and properties 
+ //for working with the user's collection
+ const usersCollection = db.collection('users')
+ 
+ export {
+  auth, 
+  db, 
+  usersCollection
+ }
