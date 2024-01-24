2e11460e0ba4a2dc8b9332b876532fc70486aafb
Ihliana <Ihnatco@protonmail.com>
2024-01-24 12:52:55 +0100

    update router/index.js
    creating named router links and configuring the links with 'linkExactActiveClass'

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,23 +2,34 @@
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <a id="title" class="text-white font-bold uppercase text-2xl mr-4" href="#">Music</a>
+      <router-link
+        id="title"
+        class="text-white font-bold uppercase text-2xl mr-4"
+        :to="{ name: 'home' }"
+        exact-active-class="no-active"
+        >Music</router-link
+      >
 
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
+          <li>
+            <router-link class="px-2 text-white" :to="{ name: 'about' }">About</router-link>
+          </li>
+
           <li v-if="!userStore.userLoggedIn">
             <a id="login" class="px-2 text-white" href="#" @click.prevent="toggleAuthModal"
               >Login / Register</a
             >
           </li>
+
           <template v-else>
             <li>
-              <a class="px-2 text-white" href="#">Manage</a>
+              <router-link class="px-2 text-white" :to="{ name: 'manage' }">Manage</router-link>
             </li>
             <li>
-              <a class="px-2 text-white" href="#" @click.prevent = 'userStore.signOut'>Logout</a>
+              <a class="px-2 text-white" href="#" @click.prevent="userStore.signOut">Logout</a>
             </li>
           </template>
         </ul>
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import About from '@/views/AboutView.vue'
+import Manage from '@/views/Manage.vue'
 
 let routes = [
   {
+    name: 'home', 
     path: '/',
     component: Home
   },
   {
+    name: 'about',
     path: '/about',
     component: About
+  },
+  {
+    name: 'manage',
+    path: '/manage',
+    component: Manage
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  routes,
+  linkExactActiveClass: 'text-yellow-500'
 })
 
 export default router
