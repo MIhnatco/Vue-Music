05d6338c66cd6596bf71cdd95b16e603a9ede77e
Ihliana <Ihnatco@protonmail.com>
2024-02-21 07:29:32 +0100

    Update
    more translation updates

--- src/views/Manage.vue
+++ src/views/Manage.vue
@@ -9,18 +9,20 @@
       <div class="col-span-2">
         <div class="bg-white rounded border border-gray-200 relative flex flex-col">
           <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
-            <span class="card-title">My Songs</span>
+            <span class="card-title">{{ $t('manage.my_songs') }}</span>
             <i class="fa fa-compact-disc float-right text-green-400 text-2xl"></i>
           </div>
           <div class="p-6">
             <!-- Composition Items -->
 
-            <composition-item v-for="(song, i) in songs" :key="song.docID" 
-                :song="song" 
-                :updateSong="updateSong" 
-                :index="i"
-                :removeSong = 'removeSong'
-             />
+            <composition-item
+              v-for="(song, i) in songs"
+              :key="song.docID"
+              :song="song"
+              :updateSong="updateSong"
+              :index="i"
+              :removeSong="removeSong"
+            />
           </div>
         </div>
       </div>
@@ -56,11 +58,11 @@ export default {
     updateSong(i, values) {
       this.songs[i].modified_name = values.modified_name
       this.songs[i].genre = values.genre
-    }, 
-    removeSong(i){
-      this.songs.splice(i,1);
-    }, 
-    addSong(document){
+    },
+    removeSong(i) {
+      this.songs.splice(i, 1)
+    },
+    addSong(document) {
       const song = {
         ...document.data(),
         docID: document.id
