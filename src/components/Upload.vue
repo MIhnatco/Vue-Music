18443b586117492329c1bc6bccad2897ee795131
Ihliana <Ihnatco@protonmail.com>
2024-02-27 04:09:01 +0100

    update Upload.vue
    Handling offline uploads

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -65,6 +65,17 @@ export default {
           return
         }
 
+        if (!navigator.onLine) {
+          this.uploads.push({
+            task: {},
+            current_progress: 100,
+            name: file.name,
+            variant: 'bg-red-400',
+            icon: 'fas fa-times',
+            text_class: 'text-red-400'
+          })
+        }
+
         const storageRef = storage.ref() //'music2-e43ef.appspot.com'
         const songsRef = storageRef.child(`songs/${file.name}`) //'music2-e43ef.appspot.com/songs/example.mp3'
         const task = songsRef.put(file)
